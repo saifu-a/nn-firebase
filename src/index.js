9fcbfe2a352a175a3f43d8368f91f1e4c8b9a31b
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-12-04 22:25:28 +0530

    add real time data collection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import {
-    getFirestore, collection, getDocs,
+    getFirestore, collection, onSnapshot,
     addDoc, deleteDoc, doc
 } from 'firebase/firestore';
 
@@ -22,18 +22,14 @@ const db = getFirestore();
 // collection ref
 const colRef = collection(db, 'books')
 
-// get collection data
-getDocs(colRef)
-    .then((snapshot) => {
-        let books = [];
-        snapshot.docs.forEach((doc) => {
-            books.push({ ...doc.data(), id: doc.id })
-        })
-        console.log(books);
-    })
-    .catch(err => {
-        console.log(err.message);
+// real time collection data
+onSnapshot(colRef, (snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+        books.push({ ...doc.data(), id: doc.id })
     });
+    console.log(books);
+});
 
 // adding documents
 const addBookForm = document.querySelector('.add');
